a123c33d40bd391d060fc933f2a990dfe72d951b
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-25 10:06:33 +0800

    Create student information model

---
+++ app/Models/StudentInformation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentInformation extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'student_id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+    protected $table = 'student_information';
+
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'last_name',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+}
