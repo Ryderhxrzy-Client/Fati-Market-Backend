dc9e0da474624a0284854da1e304ce77c4dc1643
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-25 10:06:42 +0800

    Create student verification model

---
+++ app/Models/StudentVerification.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentVerification extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'student_id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+    protected $table = 'student_verification';
+
+    protected $fillable = [
+        'user_id',
+        'link',
+        'is_verified',
+    ];
+
+    protected $casts = [
+        'is_verified' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+}
