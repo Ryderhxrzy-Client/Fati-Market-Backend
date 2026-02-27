044ccf0d88542ad87607e3e72cf26f3e4143ca47
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-27 12:47:06 +0800

    add protected routes thaht require authentcation

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\StudentManagementController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes (require authentication)
+Route::middleware('auth:sanctum')->group(function () {
+    // Admin student management routes
+    Route::prefix('admin/students')->group(function () {
+        Route::get('/', [StudentManagementController::class, 'getAllStudents']);
+        Route::get('/pending', [StudentManagementController::class, 'getPendingStudents']);
+        Route::put('/{user_id}/approve', [StudentManagementController::class, 'approveStudent']);
+        Route::put('/{user_id}/decline', [StudentManagementController::class, 'declineStudent']);
+        Route::get('/{user_id}', [StudentManagementController::class, 'getStudentDetails']);
+    });
+});
