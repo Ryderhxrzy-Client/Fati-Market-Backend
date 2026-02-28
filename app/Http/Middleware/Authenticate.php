8f0c31c1c6e273bf5ed3cf70e22569b004134313
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-28 15:40:34 +0800

    crate authenticate middleware

---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     */
+    protected function redirectTo($request)
+    {
+        if ($request->expectsJson()) {
+            return null;
+        }
+
+        return route('login');
+    }
+
+    /**
+     * Handle an unauthenticated user.
+     */
+    protected function unauthenticated($request, array $guards)
+    {
+        if ($request->expectsJson()) {
+            abort(401, 'Unauthenticated.');
+        }
+
+        parent::unauthenticated($request, $guards);
+    }
+}
