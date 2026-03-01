b6b4ffad087920f407e8a32fb660e3d433490315
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-03-01 13:10:45 +0800

    modify

--- routes/channels.php
+++ routes/channels.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-// Authorize any authenticated user (admin or student) to listen to item messages channel
-Broadcast::channel('item.{itemId}', function ($user, $itemId) {
-    return (bool) $user && in_array($user->role, ['admin', 'student']);
+// Authorize any authenticated user to listen to item messages channel
+Broadcast::channel('item.{itemId}', function ($user) {
+    return (bool) $user;
 });
 
 // Authorize any authenticated user to listen to conversation channels
-Broadcast::channel('conversation.{userId1}.{userId2}', function ($user, $userId1, $userId2) {
-    return (bool) $user && in_array($user->role, ['admin', 'student']);
+Broadcast::channel('conversation.{userId1}.{userId2}', function ($user) {
+    return (bool) $user;
 });
